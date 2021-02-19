f44e295283df5490b49c07ae05e1c8faa2ebb8bb
Erjankydyrov <erjankydyrov29@gmail.com>
2021-02-19 15:27:26 +0600

    added addEventListener

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 let button = document.querySelector("button");
 let input1 = document.querySelector("#name");
 let input2 = document.querySelector("#massage");
+let input3 = document.querySelector("#color");
+
+button.addEventListener("click", function() {
+    console.log(input1.value);
+    console.log(input2.value);
+    console.log(input3.value);
+})
